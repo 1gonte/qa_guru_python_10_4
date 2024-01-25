19ebc5300b413389a26ae30faab50c019ae0ca61
Georgii Sergeev <g.sergeev@big3.ru>
2024-01-25 16:36:53 +0300

    Complete test_registration_form

--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -1,54 +1,58 @@
-from selene import browser, be, have
+import os
+from selene import browser, be, have, by
 
 
 def test_fill_registration_form():
 
-    # 1. open form
-    browser.open('https://demoqa.com/automation-practice-form')
+    browser.open('automation-practice-form')
 
-    # 2. find and type in field First Name
-    browser.element('#firstname').should(be.blank).type('Georgii')
+    browser.driver.execute_script("document.querySelector('#fixedban').remove();")
+    browser.driver.execute_script("document.querySelector('footer').remove();")
 
-    # 3. find and type in field Last Name
-    browser.element('#lastname').should(be.blank).type('Sergeev')
-
-    # 4. find and type in field Email
+    browser.element('#firstName').type('Georgii')
+    browser.element('#lastName').should(be.blank).type('Sergeev')
     browser.element('#userEmail').should(be.blank).type('example@gmail.com')
-
-    # 5. find and toggle check-box Gender
-    browser.element('#gender-radio-1').click()
-
-    # 6. find and type in field Mobile
+    browser.element("[for='gender-radio-1']").click()
     browser.element('#userNumber').should(be.blank).type('0123456789')
 
-    # 7. find and fill field Date of Birth
-    browser.element('#dateOfBirthInput').click().\
-        element('.react-datepicker__month-select').click().\
-        element('.react-datepicker__month-select>[value="1"]').click()
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__year-select').click()
+    browser.element(".react-datepicker__year-select").click().element(
+        by.text("2001")
+    ).click()
+    browser.element(".react-datepicker__month-select").click().element(
+        by.text("February")
+    ).click()
+    browser.element(".react-datepicker__day--016").click()
 
-    browser.element('.react-datepicker__year-select').click().type('2001').press_enter()
+    browser.element('#subjectsInput').should(be.blank).type('Computer Science')\
+        .press_enter()
 
-    browser.element('.react-datepicker__day--016').click()
+    browser.element('[for="hobbies-checkbox-1"]').click()
+    browser.element('[for="hobbies-checkbox-2"]').click()
+    browser.element('[for="hobbies-checkbox-3"]').click()
 
-    # 8. find and type in field Subjects
-    browser.element('#subjectsContainer').should(be.blank).type('Computer Science').press_enter()
+    browser.element('#uploadPicture.form-control-file').send_keys(
+        os.path.abspath('image.png'))
 
-    # 9. find and toggle check-box Hobbies
-    browser.element('#hobbies-checkbox-1').click()
-    browser.element('#hobbies-checkbox-2').click()
-    browser.element('#hobbies-checkbox-3').click()
-
-    # 10. find and fill the Picture field
-
-    # 11. find and fill Current Address
     browser.element('#currentAddress').should(be.blank).type('some random address')
 
-    # 12. find and choose State and City
-    browser.element('#state').type('NCR').press_enter()
-    browser.element('#city').type('Delhi').press_enter()
+    browser.element("#state").should(be.clickable).click()
+    browser.element(by.text('NCR')).should(be.clickable).click()
+    browser.element("#city").should(be.clickable).click()
+    browser.element(by.text('Delhi')).should(be.clickable).click()
 
-    # 13. zoom out the page so the 'submit' btn is visible
-    browser.execute_script("document.body.style.zoom='65%'")
+    browser.element('#submit').should(be.clickable).click()
 
-    # 14. press Submit
-    browser.element('#submit').click()
+    browser.element('.table').all('td:nth-child(2)').should(
+        have.texts('Georgii Sergeev',
+                   'example@gmail.com',
+                   'Male',
+                   '0123456789',
+                   '16 February,2001',
+                   'Computer Science',
+                   'Sports, Reading, Music',
+                   'image.png',
+                   'some random address',
+                   'NCR Delhi'))
+
