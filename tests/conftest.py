6226219d20377a74437dd7e299cead088283654d
Georgii Sergeev <g.sergeev@big3.ru>
2024-01-25 16:37:23 +0300

    Add conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from selene import browser
+
+
+@pytest.fixture(scope='function', autouse=True)
+def browser_management():
+
+    browser.config.base_url = 'https://demoqa.com/'
+    browser.config.timeout = 6.0
+    browser.config.window_height = 1000
+    browser.config.window_width = 1500
+
+
+    yield
+
+    browser.quit()
